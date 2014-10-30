35d64b0bd3ee592a4b336e384333dd45e877cfd3
yasmindogan <yasmindogan@msn.com>
2014-10-30 23:39:14 +0300

    Create simple.scala

---
+++ HW-01/simple.scala
@@ -0,0 +1,9 @@
+object simple {
+//yasemin dogan 170201054
+val array=Array(10,25,30)
+   for ( i <- 0 to (array.length - 1) ) {
+         if (array(i)%2==0) println(array(i)*2)
+         else
+           println(array(i)*3)
+      }
+}
