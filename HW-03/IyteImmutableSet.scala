c6076143182ef6e56d93314488047e7da299cd7f
yasmindogan <yasmindogan@msn.com>
2014-11-24 23:59:06 +0200

    Create IyteImmutableSet.scala

---
+++ HW-03/IyteImmutableSet.scala
@@ -0,0 +1,74 @@
+class IyteImmutableSet (val arraybuffer:Array[Int],val initalSize:Int,val initalindex:Int){
+var arrayBuffer =arraybuffer
+var newinitalSize=initalSize
+var newinitalindex= initalindex
+
+var searchValued : Int = 0
+
+def add(element:Int):IyteImmutableSet={
+if(!contains(element)){
+if(initalindex<= initalSize ){
+arrayBuffer (initalindex)=element
+newinitalindex += 1
+
+}
+else{
+arrayBuffer (initalindex )=element
+newinitalindex += 1
+
+}
+}
+
+
+new IyteImmutableSet(arrayBuffer,initalSize,initalindex)
+}
+def contains(sValue:Int):Boolean = {
+// val flag=0
+//Binary search algorithm
+var inital = 0
+var last = newinitalindex -1
+
+
+while (inital <= last){
+
+searchValued = (inital + last)/2;
+
+if (arrayBuffer(searchValued ) == sValue){
+return true
+}
+else if (arrayBuffer (searchValued ) > sValue){
+last = searchValued - 1;
+}
+else{
+inital = searchValued + 1;
+}
+}
+return false
+}
+override def toString:String={
+var result= ""
+
+if(initalindex != 0){
+
+for(i<-0 to initalindex -1 ){
+result += arrayBuffer (i).toString + ", "
+}
+}
+else{
+
+}
+result.dropRight(1)
+}
+
+}
+object IyteImmutableSet{
+
+def apply() = {
+var arrayBuffer =new Array[Int](20)
+//var arrayinner=new Array[Int](20)
+var initalSize=10
+var initalindex= 0
+new IyteImmutableSet(arrayBuffer,initalSize,initalindex)
+}
+
+}
